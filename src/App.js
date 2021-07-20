de8f556ea02a040fc335b860448e11053b482caf
jaimegsandoval <jaime.devs@gmail.com>
2021-07-20 13:58:49 -0700

    added percent button component & styles for small mobile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/header/Header';
 import BillInput from './components/bill-total-input/BillInput';
+import PercentageButtons from './components/percentage-buttons/PercentageButtons';
 import './sass/main.scss';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <div className="content-container">
         <main className="main-container">
           <BillInput />
+          <PercentageButtons />
         </main>
       </div>
     </>
